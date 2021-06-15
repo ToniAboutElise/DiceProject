ecfa4559dac07847f38e8f1c72743673e6bfef6a
darktoni94 <tonidominguezreig@gmail.com>
2021-06-15 23:08:54 +0200

    Uploads prototype

---
+++ Assets/Scripts/DiceManager.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceManager : MonoBehaviour
+{
+    public List<Rigidbody> dicesRB;
+    public Text infoText;
+    public GameObject capsule;
+    public GameObject dice;
+
+    public GameObject diceD3;
+    public GameObject diceD4;
+    public GameObject diceD6;
+    public GameObject diceD8;
+    public GameObject diceD10;
+    public GameObject diceD12;
+    public GameObject diceD20;
+    public Transform[] diceSpawnTransform;
+    public List<DraggableDice> diceList;
+
+    public string currentDiceColor = "white";
+
+    protected void DiceAppearToPoint()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            GameObject instance = Instantiate(dice);
+            touchPosition.z = dice.transform.position.z;
+            instance.transform.localPosition = touch.position;
+            infoText.text = touch.position.ToString();
+        }
+    }
+
+    public void SpawnDice(string dice)
+    {
+        GameObject diceInstance = null;
+        switch (dice)
+        {
+            case "D3":
+                diceInstance = Instantiate(diceD3);
+                break;
+            case "D4":
+                diceInstance = Instantiate(diceD4);
+                break;
+            case "D6":
+                diceInstance = Instantiate(diceD6);
+                break;
+            case "D8":
+                diceInstance = Instantiate(diceD8);
+                break;
+            case "D10":
+                diceInstance = Instantiate(diceD10);
+                break;
+            case "D12":
+                diceInstance = Instantiate(diceD12);
+                break;
+            case "D20":
+                diceInstance = Instantiate(diceD20);
+                break;
+        }
+
+        int rand = Random.Range(0, diceSpawnTransform.Length);
+
+        diceInstance.transform.position = diceSpawnTransform[rand].position;
+        diceList.Add(diceInstance.GetComponent<DraggableDice>());
+        ChangeDiceColor(currentDiceColor);
+    }
+
+    public void ChangeDiceColor(string color)
+    {
+        bool textureHasBeenSet = false;
+        switch (color)
+        {
+            case "white":
+                foreach(DraggableDice dd in diceList)
+                {
+                    dd.GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", dd.diceTextures.white);
+                    if(textureHasBeenSet == false)
+                    {
+                        currentDiceColor = "white";
+                        textureHasBeenSet = true;
+                    }
+                }
+                break;
+            case "red":
+                foreach (DraggableDice dd in diceList)
+                {
+                    dd.GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", dd.diceTextures.red);
+                    if (textureHasBeenSet == false)
+                    {
+                        currentDiceColor = "red";
+                        textureHasBeenSet = true;
+                    }
+                }
+                break;
+            case "black":
+                foreach (DraggableDice dd in diceList)
+                {
+                    dd.GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", dd.diceTextures.black);
+                    if (textureHasBeenSet == false)
+                    {
+                        currentDiceColor = "black";
+                        textureHasBeenSet = true;
+                    }
+                }
+                break;
+            case "alien":
+                foreach (DraggableDice dd in diceList)
+                {
+                    dd.GetComponent<MeshRenderer>().material.SetTexture("_BaseMap", dd.diceTextures.alien);
+                    if (textureHasBeenSet == false)
+                    {
+                        currentDiceColor = "alien";
+                        textureHasBeenSet = true;
+                    }
+                }
+                break;
+        }
+    }
+
+    public void ClearDices()
+    {
+        DraggableDice[] dices = FindObjectsOfType<DraggableDice>();
+
+        foreach(DraggableDice d in dices)
+        {
+            Destroy(d.gameObject);
+        }
+        diceList.Clear();
+    }
+
+    private void Update()
+    {
+        DiceAppearToPoint();
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeDiceColor("white");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeDiceColor("red");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeDiceColor("black");
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/DraggableDice.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableDice : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    private int randX;
+    private int randY;
+    private int randZ;
+
+    protected bool canRotate = false;
+    protected bool canRotateWithAcceleration = true;
+    protected bool waitingForSingleRoll = false;
+
+    public DiceTextures diceTextures;
+
+    [System.Serializable]
+    public struct DiceTextures
+    {
+        public Texture white;
+        public Texture red;
+        public Texture black;
+        public Texture alien;
+    }
+
+    void OnMouseDown()
+    {
+        if(waitingForSingleRoll == false)
+        { 
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+        else
+        {
+            StartCoroutine(RollDice());
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        //StartCoroutine(RollDice());
+        StartCoroutine(SingleRoll());
+    }
+
+    protected IEnumerator SingleRoll()
+    {
+        waitingForSingleRoll = true;
+        yield return new WaitForSeconds(0.4f);
+        waitingForSingleRoll = false;
+    }
+
+    protected IEnumerator RollDice()
+    {
+        canRotateWithAcceleration = false;
+
+        randX = Random.Range(5, 20);
+        randY = Random.Range(5, 20);
+        randZ = Random.Range(5, 20);
+
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
+        yield return new WaitForSeconds(0.2f);
+        canRotate = true;
+        //transform.Rotate(new Vector3(90 * randX, 90 * randY, 0), Space.Self);
+        //transform.localRotation = Quaternion.Lerp(transform.localRotation, new Quaternion(90 * randX, 90 * randY, 90 * randZ, transform.localRotation.w), 4 * Time.deltaTime);
+        yield return new WaitForSeconds(0.3f);
+        canRotate = false;
+        canRotateWithAcceleration = true;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.A) && canRotateWithAcceleration == true)
+        {
+            StartCoroutine(RollDice());
+        }
+#endif
+
+        if(Input.acceleration.x > 3f && canRotateWithAcceleration == true)
+        {
+            StartCoroutine(RollDice());
+        }
+
+        if(canRotate == true)
+        transform.Rotate(new Vector3(90*Time.deltaTime * randX, 90*Time.deltaTime * randY, 90*Time.deltaTime * randZ), Space.Self);
+
+        if(GetComponent<Rigidbody>().velocity.x > 3f && canRotate == true && canRotateWithAcceleration == true)
+        {
+            StartCoroutine(RollDice());
+        }
+    }
+
+}
