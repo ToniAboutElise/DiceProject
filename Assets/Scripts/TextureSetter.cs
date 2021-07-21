cc5e8dbecf2c486f324ac5e0fe5282e4a4ce0ca2
darktoni94 <tonidominguezreig@gmail.com>
2021-07-21 17:03:11 +0200

    Fully implements background script

---
+++ Assets/Scripts/TextureSetter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureSetter : MonoBehaviour
+{
+    public Renderer targetRenderer;
+    public Texture2D texture;
+
+    public void SetTexture()
+    {
+        targetRenderer.material.SetTexture("_BaseMap", texture);
+    }
+}
